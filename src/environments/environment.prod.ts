d1e88686cba63fb6adf4b8f69db0e1a694a2e5cc
PaoloFumachi <cesarfumachi2002@gmail.com>
2026-03-30 18:52:21 -0500

    najdnc

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 // frontend_dsi6/src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://backendvina-production.up.railway.app/api',
+  apiUrl: 'https://proyectovi-abackend-production.up.railway.app/api',
   // ✅ Para producción, la URL debe apuntar a tu dominio real
-  yapeWebhookUrl: 'https://backendvina-production.up.railway.app/api/yape/webhook',
+  yapeWebhookUrl: 'https://proyectovi-abackend-production.up.railway.app/api/yape/webhook',
   logLevel: 'error',
-  apiUrlPHP: 'https://sunatphp-production.up.railway.app'
+  apiUrlPHP: 'https://phpsunat-production.up.railway.app'
 };
